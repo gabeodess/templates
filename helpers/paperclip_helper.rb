6d03f79512aaf550b3fe54dc3a32441fc41f97c3
Gabe Odess <gabeodess@gmail.com>
2010-03-30 09:49:04 -0400

    fixed link to paperclip

--- helpers/paperclip_helper.rb
+++ helpers/paperclip_helper.rb
@@ -1,15 +1,16 @@
 module PaperclipHelper
   
   def link_to_attachment(item, attachment, options = {}, html_options = {})
-    return 'No file.' unless attachment.file?
+    my_attachment = item.send(attachment)
+    return 'No file.' unless my_attachment.file?
     style = options.delete(:style) || :original
     link_style = options.delete(:link_style) || style
     display_style = options.delete(:display_style) || style
     use_image = options.delete(:use_image) || true
     html_options[:popup] ||= true
     html_options[:id] ||= "#{item.class}_#{attachment}_#{item.id}"
-    text = IMAGE_TYPES.include?(attachment.content_type) && use_image ? image_tag(attachment.url(display_style)) : "Link to #{attachment.original_filename}"
-    return link_to(text, attachment.url(link_style), html_options)
+    text = IMAGE_TYPES.include?(my_attachment.content_type) && use_image ? image_tag(my_attachment.url(display_style)) : "Link to #{my_attachment.original_filename}"
+    return link_to(text, my_attachment.url(link_style), html_options)
   end
   alias_method :link_to_paperclip, :link_to_attachment
   
