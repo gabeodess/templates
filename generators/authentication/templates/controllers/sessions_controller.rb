81d63dfc57c1c6db339c14b0aab2a091f6559066
Gabe Odess <gabeodess@gmail.com>
2011-09-19 09:58:38 -0400

    removed has_role? from session redirect

--- generators/authentication/templates/controllers/sessions_controller.rb
+++ generators/authentication/templates/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class <%= controller_name.camelcase %>Controller < ApplicationController
     <%= user_name.underscore %> = User.authenticate(params[:email], params[:password])
     if <%= user_name.underscore %>
       session[:<%= user_name.underscore %>_id] = <%= user_name.underscore %>.id
-      redirect = <%= user_name.underscore %>.has_role?(:super_admin) ? admin_url : root_url
-      redirect_to redirect, :notice => "Logged in!"
+      redirect_to :root, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid login or password"
       render "new"
